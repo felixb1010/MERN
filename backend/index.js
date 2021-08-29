20e9cfe0e77ed8d64fc077d89972338c84eb63a2
Felix <felixb@askeris.no>
2021-08-29 21:17:07 +0200

    DB

--- backend/index.js
+++ backend/index.js
@@ -4,4 +4,22 @@ import dotenv from "dotenv"
 dotenv.config()
 const MongoClient = mongodb.MongoClient
 
-const
+const port = process.env.PORT || 8000
+
+MongoClient.connect(
+    process.env.RESTREVIEWS_DB_URI,
+    {
+        maxPoolSize: 50,
+        wtimeoutMS: 2500,
+        useNewUrlParse: true ,
+    }
+)
+.catch(err => {
+    console.error(err.stack)
+    process.exit(1)
+})
+.then(async client => {
+    app.listen(port, () => {
+        console.log(`listening on port ${port}`)
+    })
+})
